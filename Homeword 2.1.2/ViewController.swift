efec975d9493bf3d83a3dc3ad1d030e4672aed3f
Darya Yarovaya <yarovayadarya@mail.ru>
2021-05-18 15:27:49 +0300

    Added minimal functionality

--- Homeword 2.1.2/ViewController.swift
+++ Homeword 2.1.2/ViewController.swift
@@ -7,15 +7,56 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var currentLight = CurrentLight.red
+    let lightOn: CGFloat = 1
+    let lightOff: CGFloat = 0.3
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
+        
+        redView.alpha = lightOff
+        yellowView.alpha = lightOff
+        greenView.alpha = lightOff
+        
+        redView.layer.cornerRadius = redView.frame.size.width/2
+        yellowView.layer.cornerRadius = yellowView.frame.size.width/2
+        greenView.layer.cornerRadius = greenView.frame.size.width/2
+        
+        startButton.layer.cornerRadius = 20
     }
 
-
+    @IBAction func pressedButton() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            if greenView.alpha > 0.3 {
+                greenView.alpha = lightOff
+            }
+            redView.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightOff
+            yellowView.alpha = lightOn
+            currentLight = .green
+        case .green:
+            yellowView.alpha = lightOff
+            greenView.alpha = lightOn
+            currentLight = .red
+        }
+    }
+    
 }
 
